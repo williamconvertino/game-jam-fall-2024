7334fddc1e9c70328e15828a8aba10a082bde8d0
Jacob Lee <53345177+Jacob-W-Lee@users.noreply.github.com>
2024-10-05 22:42:29 -0400

    added grabber

---
+++ Assets/Scripts/Enemies/Grabber.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Grabber : MonoBehaviour
+{
+    public GameObject target;
+
+    public float pursueRange;
+    public float closeRange;
+    public float acceleration;
+    public float decceleration;
+    private Vector2 prev_error;
+
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private string[] _tagsToIgnore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // need to give it a player to target
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 error = (target.transform.position - transform.position);
+        Vector2 error_derivative = error - prev_error;
+        float kp = 1f;
+        float kd = 1f;
+        float dt = 1 / 60f;
+        // Accelerate towards target
+        rb.AddForce((kp * error) + (kd * error_derivative / dt));
+        prev_error = error;
+        return;
+
+    }
+}
